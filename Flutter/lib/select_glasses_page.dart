67f1aa9f7394a1f1bd9562cd7160b6e1cc6d3ad1
JisforJelly <90131462+JisforJelly@users.noreply.github.com>
2022-06-08 15:32:40 +0900

    Update select_glasses_page.dart

--- Flutter/lib/select_glasses_page.dart
+++ Flutter/lib/select_glasses_page.dart
@@ -74,9 +74,9 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://cdn-icons-png.flaticon.com/512/3617/3617007.png', //1st
-                          fit: BoxFit.cover,
+                        child: Image.asset(
+                          'images/1stglasses.jpg', //1st
+                          fit: BoxFit.contain,
                         ),
                       ),
                     ),
@@ -93,8 +93,8 @@ class SelectGlassesPage extends StatelessWidget {
                             width: 3,
                           )
                       ),
-                      child: Image.network(
-                        'https://static.zennioptical.com/production/products/general/10/12/101235-eyeglasses-front-view.jpg', //2nd
+                      child: Image.asset(
+                        'images/2ndglasses.jpg', //2nd
                         fit: BoxFit.contain,
                       ),
                     ),
@@ -111,8 +111,8 @@ class SelectGlassesPage extends StatelessWidget {
                             width: 3,
                           )
                       ),
-                      child: Image.network(
-                        'https://blackeyewear.com/wp-content/uploads/2019/10/WES_BRFL_G_FRONT-680x238.jpg', //3rd
+                      child: Image.asset(
+                        'images/3rdglasses.jpg', //3rd
                         fit: BoxFit.contain,
                       ),
                     ),
@@ -134,8 +134,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://images-na.ssl-images-amazon.com/images/I/415mGS-NKnL._AC_UL1024_.jpg', //4th
+                        child: Image.asset(
+                          'images/4thglasses.jpg', //4th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -152,8 +152,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://img.ebdcdn.com/product/frame/white/pm0136_3.jpg?im=Resize,width=600,height=300,aspect=fill;UnsharpMask,sigma=1.0,gain=1.0', //5th
+                        child: Image.asset(
+                          'images/5thglasses.jpg', //5th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -170,8 +170,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://www.directsight.co.uk/resources/version/1467204059555909', //6th
+                        child: Image.asset(
+                          'images/6thglasses.jpg', //6th
                           fit: BoxFit.contain,
                         ),
                       )
@@ -193,8 +193,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://static.zennioptical.com/production/products/general/20/84/208425-eyeglasses-front-view.jpg', //7th
+                        child: Image.asset(
+                          'images/7thglasses.jpg', //7th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -211,8 +211,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://img.ebdcdn.com/product/front/white/mt6567.jpg?im=Resize,width=600,height=300,aspect=fill;UnsharpMask,sigma=1.0,gain=1.0', //8th
+                        child: Image.asset(
+                          'images/8thglasses.jpg', //8th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -229,8 +229,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://m.media-amazon.com/images/I/61Wh+37QNhL._AC_SL1500_.jpg', //9th
+                        child: Image.asset(
+                          'images/9thglasses.jpg', //9th
                           fit: BoxFit.contain,
                         ),
                       )
@@ -252,8 +252,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://sc01.alicdn.com/kf/Hf58057bbe3054c1c81903ac0f6a03ef4Q/237330106/Hf58057bbe3054c1c81903ac0f6a03ef4Q.jpg_.webp', //10th
+                        child: Image.asset(
+                          'images/10thglasses.jpg', //10th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -270,8 +270,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://cloudfront.zeelool.com/media/catalog/product/1/_/1_276_2.jpg', //11th
+                        child: Image.asset(
+                          'images/11thglasses.jpg', //11th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -288,8 +288,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://directsight.co.uk/resources/version/1495014027399658', //12th
+                        child: Image.asset(
+                          'images/12thglasses.jpg', //12th
                           fit: BoxFit.contain,
                         ),
                       )
@@ -311,8 +311,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://cloudfront.zeelool.com/media/catalog/product/e/f/eff97f5b2591e4c2fd37043cea4b9eeb_1.jpg', //13th
+                        child: Image.asset(
+                          'images/13thglasses.jpg', //13th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -329,8 +329,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://i.shgcdn.com/df4060d3-7797-4a51-b349-7eb739558920/-/format/auto/-/preview/3000x3000/-/quality/lighter/', //14th
+                        child: Image.asset(
+                          'images/14thglasses.jpg', //14th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -347,8 +347,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://glassesshop-res.cloudinary.com/c_fill,f_auto,fl_lossy,q_auto,w_800,h_400,c_pad/products/202112/61adbf7e62f41.jpg', //15th
+                        child: Image.asset(
+                          'images/15thglasses.jpg', //15th
                           fit: BoxFit.contain,
                         ),
                       )
@@ -370,8 +370,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://img.ebdcdn.com/product/frame/white/pm0136_3.jpg?im=Resize,width=600,height=300,aspect=fill;UnsharpMask,sigma=1.0,gain=1.0', //16th
+                        child: Image.asset(
+                          'images/16thglasses.jpg', //16th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -388,8 +388,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://d1u18ka6p421dn.cloudfront.net/glasses-images/30776777-front-940x529.jpg', //17th
+                        child: Image.asset(
+                          'images/17thglasses.jpg', //17th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -406,8 +406,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://s3-eu-west-1.amazonaws.com/specsavers-loveglasses/uploads/loveglasses/frames/30377172-Replay-32-Front.jpg', //18th
+                        child: Image.asset(
+                          'images/18thglasses.jpg', //18th
                           fit: BoxFit.contain,
                         ),
                       )
@@ -429,8 +429,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://s3-eu-west-1.amazonaws.com/specsavers-loveglasses/uploads/loveglasses/frames/30400207-Love-Moschino-LM-07-Front.jpg', //19th
+                        child: Image.asset(
+                          'images/19thglasses.jpg', //19th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -447,8 +447,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://cloudfront.zeelool.com/media/catalog/product/1/_/1_849_1.jpg', //20th
+                        child: Image.asset(
+                          'images/20thglasses.jpg', //20th
                           fit: BoxFit.contain,
                         ),
                       ),
@@ -465,8 +465,8 @@ class SelectGlassesPage extends StatelessWidget {
                               width: 3,
                             )
                         ),
-                        child: Image.network(
-                          'https://m.media-amazon.com/images/I/61iY+yuOlLL._AC_UL1500_.jpg', //21th
+                        child: Image.asset(
+                          'images/21stglasses.jpg', //21st
                           fit: BoxFit.contain,
                         ),
                       )
